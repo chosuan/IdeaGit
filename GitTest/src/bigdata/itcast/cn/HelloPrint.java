44ffded7e250912e29355ba5a937b6d69409f6d9
ddxl <651675921@qq.com>
2020-06-16 21:39:03 +0800

    version3

--- GitTest/src/bigdata/itcast/cn/HelloPrint.java
+++ GitTest/src/bigdata/itcast/cn/HelloPrint.java
@@ -5,6 +5,7 @@ public class HelloPrint {
     public static void main(String[] args) {
         System.out.println("this is version1, i love china");
         System.out.println("this is version2, i love itcast");
+        System.out.println("this is version2, i love heima");
 
     }
 
